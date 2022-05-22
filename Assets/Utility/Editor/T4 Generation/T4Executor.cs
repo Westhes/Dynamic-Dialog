a3fb1dcd88a7696f9763d3a6f09d0dfa2ed2e572
Sebas West <s.westheim62@gmail.com>
2022-05-22 16:02:54 +0200

    Created a demo and found, and modified, a helper script for the use of T4 code template generation.

---
+++ Assets/Utility/Editor/T4 Generation/T4Executor.cs
@@ -0,0 +1,50 @@
+using System.IO;
+using System.Reflection;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace GameName.Utility
+{
+    public class T4Executor : Editor
+    {
+        static string className = "GeneratedTestClass";
+
+        [MenuItem("Blackboard/GenerateKeysExample")]
+        public static void Execute()
+        {
+            Assets.Utility.Editor.Template.BlackboardKeyGenerator gen = new()
+            {
+                NameSpace = "GameName.Utility",
+                ClassName = className,
+                KeyNames = new string[] { "Hello", "World", "test" },
+            };
+            string content = gen.TransformText();
+
+            if (!TryGetActiveFolderPath(out string currentDirectory))
+            {
+                Debug.LogError("File generation aborted, folder could not be found!");
+                return;
+            }
+            string outputPath = currentDirectory + Path.AltDirectorySeparatorChar + className + ".cs";
+            Debug.Log($"File written to: {outputPath}\n{content}");
+            File.WriteAllText(outputPath, content);
+            AssetDatabase.Refresh();
+        }
+
+        private static bool TryGetActiveFolderPath(out string path)
+        {
+            var _tryGetActiveFolderPath = typeof(ProjectWindowUtil).GetMethod("TryGetActiveFolderPath", BindingFlags.Static | BindingFlags.NonPublic);
+            object[] args = new object[] { null };
+            bool found = (bool)_tryGetActiveFolderPath.Invoke(null, args);
+            path = (string)args[0];
+            return found;
+        }
+
+        private static string ProjectDirectory()
+        {
+            return Application.dataPath.Substring(0, Application.dataPath.LastIndexOf('/') + 1);
+        }
+    }
+}
---
+++ Assets/Utility/Editor/T4 Template/GeneratedTestClass.cs
@@ -0,0 +1,13 @@
+
+namespace GameName.Utility
+{
+	public class GeneratedTestClass
+	{
+		public const int Hello = -137262718;
+		public const int World = -71942585;
+		public const int test = -662733300;
+	}
+}
+
+
+
